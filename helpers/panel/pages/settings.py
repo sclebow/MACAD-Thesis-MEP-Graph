43eea7ec6d3a7e478225e8a28ff404b7224db701
sclebow <sclebow@gmail.com>
2025-09-14 22:04:58 -0400

    Comment out budget input watchers in settings layout function to avoid double processing

--- helpers/panel/pages/settings.py
+++ helpers/panel/pages/settings.py
@@ -26,15 +26,15 @@ def layout_settings(settings_container, graph_controller: GraphController, DEFAU
     settings_container.append(settings_row)
 
     budget_hours_input = pn.widgets.NumberInput(name="Monthly Budget (Hours)", value=DEFAULT_SIMULATION_PARAMS["budget_hours"], step=1)
-    budget_hours_input.param.watch(lambda event: update_hours_budget(event, graph_controller), "value")
+    # budget_hours_input.param.watch(lambda event: update_hours_budget(event, graph_controller), "value")
     graph_controller.update_hours_budget(DEFAULT_SIMULATION_PARAMS["budget_hours"])
 
     budget_money_input = pn.widgets.NumberInput(name="Monthly Budget (Dollars)", value=DEFAULT_SIMULATION_PARAMS["budget_money"], step=100)
-    budget_money_input.param.watch(lambda event: update_money_budget(event, graph_controller), "value")
+    # budget_money_input.param.watch(lambda event: update_money_budget(event, graph_controller), "value")
     graph_controller.update_money_budget(DEFAULT_SIMULATION_PARAMS["budget_money"])
 
     num_weeks_to_schedule_input = pn.widgets.NumberInput(name="Weeks to Schedule", value=DEFAULT_SIMULATION_PARAMS["weeks_to_schedule"], step=1)
-    num_weeks_to_schedule_input.param.watch(lambda event: update_weeks_to_schedule(event, graph_controller), "value")
+    # num_weeks_to_schedule_input.param.watch(lambda event: update_weeks_to_schedule(event, graph_controller), "value")
     graph_controller.update_weeks_to_schedule(DEFAULT_SIMULATION_PARAMS["weeks_to_schedule"])
 
     pn.state.cache["settings_money_budget_input"] = budget_money_input
